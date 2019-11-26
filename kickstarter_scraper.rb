a8cfea592455ad302b91612facfdf9166a959969
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-26 22:20:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,6 @@
 # require libraries/modules here
 require "pry"
-require "nikogiri"
+require "nokogiri"
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
