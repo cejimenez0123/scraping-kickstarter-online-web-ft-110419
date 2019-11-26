97361000dfe9e4897f8cbfa2273a05318bac2f64
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-26 22:27:27 +0000

    Automatically backed up by Learn

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -3,6 +3,8 @@ describe "#create_project_hash" do
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
+  # location: project.css("ul.project-meta span.location-name").text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
   it "can be called on with no errors" do
     expect {create_project_hash}.to_not raise_error
   end
