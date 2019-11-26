3be8aa3e70659b142297bb912c389a5015ed2b8d
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-26 22:25:53 +0000

    Automatically backed up by Learn

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -2,6 +2,7 @@ describe "#create_project_hash" do
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
   it "can be called on with no errors" do
     expect {create_project_hash}.to_not raise_error
   end
